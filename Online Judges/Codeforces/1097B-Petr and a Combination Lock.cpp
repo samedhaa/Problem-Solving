f4587a2457428d351874c9461f860c1263a4bd97
Samed Alhajajla <41950927+samedhaa@users.noreply.github.com>
2019-01-06 15:39:02 +0300

    Backtracking solution

---
+++ Online Judges/Codeforces/1097B-Petr and a Combination Lock.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<algorithm>
+#include<map>
+
+using namespace std;
+
+int arr[16],n;
+int solve(int i, int holder)
+{
+    if(i == n)
+    {
+        if(holder== 0 or holder % 360 == 0){
+            return 1e8;
+        }
+        else
+            return -1e8;
+    }
+
+    return max(solve(i+1,holder + arr[i+1]), solve(i+1,holder - arr[i+1]));
+}
+int main()
+{
+    cin>>n;
+    for(int i=1;i<=n;i++)
+        cin>>arr[i];
+    if(solve(0,0) >= 1e8)
+        cout<<"YES";
+    else
+        cout<<"NO";
+    return 0;
+}
