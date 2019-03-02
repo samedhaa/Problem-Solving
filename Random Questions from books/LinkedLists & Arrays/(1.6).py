6bdfe2503f5b025f109d46b2647842ba12c8c7ec
Samedhaa <haasamed@gmail.com>
2019-03-02 22:04:23 +0200

    update files

---
+++ Random Questions from books/LinkedLists & Arrays/(1.6).py
@@ -0,0 +1,8 @@
+'''
+
+Given an image represtented by an NxN MATRIX,
+where each pixel in the image is 4 bytes,
+write a method to rotate the image by 90 degrees.
+
+'''
+
---
+++ Random Questions from books/LinkedLists & Arrays/(1.7).py
@@ -0,0 +1,6 @@
+'''
+
+Write an algorithm such that if an element in an MxN matrix is 0,
+it's entire row and column is set to 0.
+
+'''
