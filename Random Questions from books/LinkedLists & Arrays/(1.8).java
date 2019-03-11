a5a8222d0927705a8cc7f5c3d02f09884aaab6ad
Samedhaa <haasamed@gmail.com>
2019-03-11 10:44:32 +0200

    upload

---
+++ Random Questions from books/LinkedLists & Arrays/(1.8).java
@@ -0,0 +1,26 @@
+'''
+
+Assume you have a method isSubstring which checks if one word is a substring of 
+another, Given two strings,
+s1 and s2, write code to check if s2 is a rotation of s1 using only one call to
+isSubstring (i.e., "waterbottle" is a rotation of "erbottlewat").
+
+'''
+
+  public static boolean isRotation(final String s1, final String s2)
+  {
+    if (s1 == null || s2 == null)
+    {
+      return false;
+    }
+
+    final int len = s1.length();
+    /* check that s1 and s2 are equal length and not empty */
+    if (len == s2.length() && len > 0)
+    {
+      /* concatenate s1 and s1 within new buffer */
+      final String s1s1 = s1 + s1;
+      return s1s1.contains(s2);
+    }
+    return false;
+  }
