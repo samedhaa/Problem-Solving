c8b31b3c8a0fa265135ba3d2c3647c62d95b5f2a
Samed Alhajajla <41950927+samedhaa@users.noreply.github.com>
2019-03-11 10:40:56 +0200

    Update (1.6).py

--- Random Questions from books/LinkedLists & Arrays/(1.6).py
+++ Random Questions from books/LinkedLists & Arrays/(1.6).py
@@ -6,3 +6,28 @@ write a method to rotate the image by 90 degrees.
 
 '''
 
+public class Q6 {
+    static int[][] rotate(int[][] matrix) {
+        int n = matrix.length;
+        int[][] ret = new int[n][n];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                ret[i][j] = matrix[n-1-j][i];
+            }
+        }
+        return ret;
+    }
+
+    static void rotateInPlace(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n/2; ++i) {
+            for (int j = i; j < n-1-i; ++j) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = tmp;
+            }
+        }
+    }
+
